8b0828292baf81547117b4fcc7b21b7bc02811be
Hinal0101 <hinaljethava112002@gmail.com>
2025-06-21 17:43:47 +0530

    kadanes method

--- MaxSubArraySum3.java
+++ MaxSubArraySum3.java
@@ -11,11 +11,19 @@ public class MaxSubArraySum3 {
             }
             ms=Math.max(cs,ms);
         }
+        if(ms==0){
+            ms=Integer.MIN_VALUE;
+            for(int i=0;i<arr.length;i++){
+                if(ms<arr[i]){
+                    ms=arr[i];
+                }
+            }
+        }
 
         System.out.println("our max subarray sum is "+ms);
     }
     public static void main(String args[]){
-        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+        int arr[]={-2,-3,-4,-1,-2,-1,-5,-3};
         kadanes(arr);
     }
 }
